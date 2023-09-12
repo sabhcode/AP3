acca9d13c54a2410ae2af420ba25008cdeb9b2e7
Hannoye Corentin <corentinhnyy@gmail.com>
2023-09-12 19:09:40 +0200

    updating db.sql file

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ap3` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ap3`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ap3
@@ -102,8 +104,8 @@ CREATE TABLE `orders` (
   PRIMARY KEY (`or_uuid`),
   KEY `fk_os_uuid_idx` (`fk_os_uuid`),
   KEY `fk_us_uuid_idx` (`fk_us_uuid`),
-  CONSTRAINT `fk_os_uuid` FOREIGN KEY (`fk_os_uuid`) REFERENCES `order_states` (`os_uuid`),
-  CONSTRAINT `fk_us_uuid` FOREIGN KEY (`fk_us_uuid`) REFERENCES `users` (`us_uuid`)
+  CONSTRAINT `or_fk_os_uuid` FOREIGN KEY (`fk_os_uuid`) REFERENCES `order_states` (`os_uuid`),
+  CONSTRAINT `or_fk_us_uuid` FOREIGN KEY (`fk_us_uuid`) REFERENCES `users` (`us_uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -285,4 +287,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-12 19:02:19
+-- Dump completed on 2023-09-12 19:08:21
