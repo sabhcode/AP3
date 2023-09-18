27992204fba72b4c00ad871d9ca1d896f5643bbd
Hannoye Corentin <corentinhnyy@gmail.com>
2023-09-18 21:47:14 +0200

    updating app

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -78,7 +78,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre email a bien été vérifié');
 
         return $this->redirectToRoute('app_register');
     }
