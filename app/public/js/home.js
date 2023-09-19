8ec8c4e526495cd81ea6e1e0b3a64649b57e66c6
sabhcode <sabhtech@gmail.com>
2023-09-19 16:21:43 +0200

    maj slider 1

--- app/public/js/home.js
+++ app/public/js/home.js
@@ -1,14 +1,13 @@
-// Début Code du slider 
-
+// Début du code du slider
 let currentSlide = 0;
-const slides = document.querySelectorAll('.slider-image');
+const cards = document.querySelectorAll('.card');
 const prevButton = document.getElementById('prevButton');
 const nextButton = document.getElementById('nextButton');
 
 function showSlide(n) {
-    slides[currentSlide].style.display = 'none';
-    currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].style.display = 'block';
+    cards[currentSlide].style.display = 'none';
+    currentSlide = (n + cards.length) % cards.length;
+    cards[currentSlide].style.display = 'block';
 }
 
 function nextSlide() {
@@ -19,12 +18,10 @@ function previousSlide() {
     showSlide(currentSlide - 1);
 }
 
-// Afficher la première image au chargement de la page
+// Afficher la première carte au chargement de la page
 showSlide(currentSlide);
 
 // Gestion des événements pour les boutons de navigation
 prevButton.addEventListener('click', previousSlide);
 nextButton.addEventListener('click', nextSlide);
-
-
-// Fin Code du slider
+// Fin du code du slider
