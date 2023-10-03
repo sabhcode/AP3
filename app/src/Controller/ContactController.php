d808ff3125dc5d928f2c7f419072e4dbbc29576b
sabhcode <sabhtech@gmail.com>
2023-10-03 17:00:29 +0200

    push final contact

--- app/src/Controller/ContactController.php
+++ app/src/Controller/ContactController.php
@@ -11,6 +11,6 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
-        return $this->render('contact/index.html.twig');
+        return $this->render('contact/contact.html.twig');
     }
 }
