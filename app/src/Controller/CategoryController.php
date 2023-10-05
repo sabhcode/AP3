fe7d889b7610e50357f67c9ba10dcdaeeaf466c8
Hannoye Corentin <corentinhnyy@gmail.com>
2023-10-05 16:52:43 +0200

    fix bug

--- app/src/Controller/CategoryController.php
+++ app/src/Controller/CategoryController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class CatalogueController extends AbstractController
+class CategoryController extends AbstractController
 {
     #[Route('/catalogue', name: 'catalogue')]
     public function index(CategoryRepository $categorie, ProductRepository $product): Response
