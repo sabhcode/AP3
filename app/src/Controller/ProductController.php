0a4410696f94394b3021c236aba83ffe7352081f
mohamed <ouzammad.mohamed@gmail.com>
2023-10-05 15:48:26 +0200

    catalogue dans le header

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProduitController extends AbstractController
+class ProductController extends AbstractController
 {
     #[Route('/produit', name: 'app_produit')]
     public function index(): Response
