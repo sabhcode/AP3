71b591f7f5662f41ae761d4d321db1f74638e511
sabhcode <sabhtech@gmail.com>
2023-10-10 14:47:07 +0200

    sort by best product by category function

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -11,16 +11,28 @@ use App\Repository\CategoryRepository;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
 
-        $products = $productRepository->findAll();
+        $categories = $categoryRepository->findAll();
+
+        foreach ($categories as $category) {
+
+            $BestProductByCategory[] = $productRepository->findOneBy(['category' => $category->getUuid()], ['nbVentes' => 'DESC']);
+
+
+
+        }
+
+
+        dump($BestProductByCategory);
+        die;
 
         $bestSells = $productRepository->findBy([], ['nbVentes' => 'DESC'], 5);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'products' => $products,
+            'categories' => $categories,
             'bestSells' => $bestSells
         ]);
     }
