0706a5c53734617ee455fd988a8353f092e4fda5
sabhcode <sabhtech@gmail.com>
2023-10-12 14:11:17 +0200

    add route for product page

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    #[Route('/produit/{produit}', name: 'app_produit')]
+    public function index(Product $produit): Response
     {
-        return $this->render('produit/produit.html.twig');
+
+        return $this->render('produit/produit.html.twig', ['produit' => $produit]);
     }
 }
