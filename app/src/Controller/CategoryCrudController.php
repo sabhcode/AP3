e26b4473a409dc7af0934440cdb608fae7cc5505
sabhcode <sabhcode@gmail.com>
2023-10-17 14:56:45 +0200

    change category crud link

--- app/src/Controller/CategoryCrudController.php
+++ app/src/Controller/CategoryCrudController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/category/crud')]
+#[Route('/categorycrud')]
 class CategoryCrudController extends AbstractController
 {
     #[Route('/', name: 'app_category_crud_index', methods: ['GET'])]
