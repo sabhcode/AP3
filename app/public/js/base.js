55f18a9c29920b9caf086e1078ce82eb2e84808b
sabhcode <sabhcode@gmail.com>
2023-10-19 17:03:45 +0200

    add comment to fix issue #23

--- app/public/js/base.js
+++ app/public/js/base.js
@@ -44,15 +44,16 @@ btnsUpdateProductInCart.forEach(btn => {
     btn.addEventListener("click", function(event) {
 
         event.preventDefault();
-
+        // console.log(this)
         const productUuid = this.getAttribute("data-productUuid");
         const action = this.getAttribute("data-action");
-
+        // console.log(productUuid)
         const form = new FormData();
 
         form.append("productUuid", productUuid);
         form.append("action", action);
 
+        // console.log(form);
         fetch("ajout-produit-panier", {method: "POST", body: form});
         // window.location.reload();    
         // est en commentaire psk si je le met ici il va appliquer ça à toutes les pages (ex: home) et va recharger la page pour rien
