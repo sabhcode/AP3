addf8f74ed21644ac90fa9d1278a9bedc7d3b7e3
Hannoye Corentin <corentinhnyy@gmail.com>
2023-10-26 16:30:00 +0200

    update profil, register, login

--- lotA/src/Controller/ProfilController.php
+++ lotA/src/Controller/ProfilController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_USER")]
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
+    #[Route('/mon-profil', name: 'app_profil')]
     public function profil(): Response
     {
         return $this->render('profil/profil.html.twig');
