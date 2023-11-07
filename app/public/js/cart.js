ce053cb7a921920801facb676eca0480660bd302
Hannoye Corentin <corentinhnyy@gmail.com>
2023-11-07 22:45:32 +0100

    modify order price ttc select radio input

--- app/public/js/cart.js
+++ app/public/js/cart.js
@@ -1,1 +1,36 @@
+const radioInputs = document.querySelectorAll("input[type='radio']");
+const deliveryChoiceHomeValue = document.getElementById("delivery-choice-home-value");
+const orderPriceHTValue = document.getElementById("order-price-ht-value");
+const orderPriceTTCValue = document.getElementById("order-price-ttc-value");
 
+const formatPrice = price => {
+
+    const formatEuro = new Intl.NumberFormat("fr-FR", {style: 'currency', currency: 'EUR'});
+
+    return formatEuro.format(price);
+
+}
+
+radioInputs.forEach(radio => {
+
+    radio.addEventListener("change", function() {
+
+        if(this.id === "delivery-choice-home") {
+
+            const newOrderPriceHT = orderPriceHT + 10;
+
+            orderPriceHTValue.innerHTML = formatPrice(newOrderPriceHT);
+            orderPriceTTCValue.innerHTML = formatPrice((newOrderPriceHT) + (newOrderPriceHT * tax / 100));
+            deliveryChoiceHomeValue.hidden = false;
+
+        } else {
+
+            orderPriceHTValue.innerHTML = formatPrice(orderPriceHT);
+            orderPriceTTCValue.innerHTML = formatPrice(orderPriceHT + (orderPriceHT * tax / 100));
+            deliveryChoiceHomeValue.hidden = true;
+
+        }
+
+    });
+
+});
