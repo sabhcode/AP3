9d7481b38ee3648df21a9a08f3ec3640e0e331d9
Bastienmnr <bastienmonier@msn.com>
2023-11-14 16:36:07 +0100

    maj entité LOT B

--- app/src/Entity/Shelf.php
+++ app/src/Entity/Shelf.php
@@ -37,9 +37,25 @@ class Shelf
     #[ORM\JoinColumn(name:"building_code", referencedColumnName:"code",nullable: false)]
     private ?Building $building = null;
 
+    #[ORM\OneToMany(mappedBy: 'shelf_section_code', targetEntity: StockShelf::class)]
+    private Collection $stockShelvesSections;
+
+    #[ORM\OneToMany(mappedBy: 'shelf_row_shelf_code', targetEntity: StockShelf::class)]
+    private Collection $stockShelvesRowShelf;
+
+    #[ORM\OneToMany(mappedBy: 'shelf_modele_code', targetEntity: StockShelf::class)]
+    private Collection $stockShelvesModules;
+
+    #[ORM\OneToMany(mappedBy: 'shelf_building_code', targetEntity: StockShelf::class)]
+    private Collection $stockShelvesBuildings;
+
     public function __construct()
     {
         $this->stockShelves = new ArrayCollection();
+        $this->stockShelvesSections = new ArrayCollection();
+        $this->stockShelvesRowShelf = new ArrayCollection();
+        $this->stockShelvesModules = new ArrayCollection();
+        $this->stockShelvesBuildings = new ArrayCollection();
     }
 
     public function getCode(): ?string
@@ -128,6 +144,126 @@ class Shelf
     public function setBuilding(?Building $building): static
     {
         $this->building = $building;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StockShelf>
+     */
+    public function getStockShelvesSections(): Collection
+    {
+        return $this->stockShelvesSections;
+    }
+
+    public function addStockShelvesSection(StockShelf $stockShelvesSection): static
+    {
+        if (!$this->stockShelvesSections->contains($stockShelvesSection)) {
+            $this->stockShelvesSections->add($stockShelvesSection);
+            $stockShelvesSection->setShelfSectionCode($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStockShelvesSection(StockShelf $stockShelvesSection): static
+    {
+        if ($this->stockShelvesSections->removeElement($stockShelvesSection)) {
+            // set the owning side to null (unless already changed)
+            if ($stockShelvesSection->getShelfSectionCode() === $this) {
+                $stockShelvesSection->setShelfSectionCode(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StockShelf>
+     */
+    public function getStockShelvesRowShelf(): Collection
+    {
+        return $this->stockShelvesRowShelf;
+    }
+
+    public function addStockShelvesRowShelf(StockShelf $stockShelvesRowShelf): static
+    {
+        if (!$this->stockShelvesRowShelf->contains($stockShelvesRowShelf)) {
+            $this->stockShelvesRowShelf->add($stockShelvesRowShelf);
+            $stockShelvesRowShelf->setShelfRowShelfCode($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStockShelvesRowShelf(StockShelf $stockShelvesRowShelf): static
+    {
+        if ($this->stockShelvesRowShelf->removeElement($stockShelvesRowShelf)) {
+            // set the owning side to null (unless already changed)
+            if ($stockShelvesRowShelf->getShelfRowShelfCode() === $this) {
+                $stockShelvesRowShelf->setShelfRowShelfCode(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StockShelf>
+     */
+    public function getStockShelvesModules(): Collection
+    {
+        return $this->stockShelvesModules;
+    }
+
+    public function addStockShelvesModule(StockShelf $stockShelvesModule): static
+    {
+        if (!$this->stockShelvesModules->contains($stockShelvesModule)) {
+            $this->stockShelvesModules->add($stockShelvesModule);
+            $stockShelvesModule->setShelfModeleCode($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStockShelvesModule(StockShelf $stockShelvesModule): static
+    {
+        if ($this->stockShelvesModules->removeElement($stockShelvesModule)) {
+            // set the owning side to null (unless already changed)
+            if ($stockShelvesModule->getShelfModeleCode() === $this) {
+                $stockShelvesModule->setShelfModeleCode(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StockShelf>
+     */
+    public function getStockShelvesBuildings(): Collection
+    {
+        return $this->stockShelvesBuildings;
+    }
+
+    public function addStockShelvesBuilding(StockShelf $stockShelvesBuilding): static
+    {
+        if (!$this->stockShelvesBuildings->contains($stockShelvesBuilding)) {
+            $this->stockShelvesBuildings->add($stockShelvesBuilding);
+            $stockShelvesBuilding->setShelfBuildingCode($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStockShelvesBuilding(StockShelf $stockShelvesBuilding): static
+    {
+        if ($this->stockShelvesBuildings->removeElement($stockShelvesBuilding)) {
+            // set the owning side to null (unless already changed)
+            if ($stockShelvesBuilding->getShelfBuildingCode() === $this) {
+                $stockShelvesBuilding->setShelfBuildingCode(null);
+            }
+        }
 
         return $this;
     }
--- app/src/Entity/StockShelf.php
+++ app/src/Entity/StockShelf.php
@@ -17,12 +17,31 @@ class StockShelf
     #[ORM\ManyToOne(inversedBy: 'stockShelves')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Product $product = null;
-
     
     #[ORM\Id]
     #[ORM\ManyToOne(inversedBy: 'stockShelves')]
     #[ORM\JoinColumn(name:"shelf_code", referencedColumnName:"code", nullable: false)]
     private ?Shelf $shelf = null;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'stockShelvesSections')]
+    #[ORM\JoinColumn(name:"shelf_section_code", referencedColumnName:"section_code",nullable: false)]
+    private ?Shelf $shelf_section_code = null;
+
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'stockShelvesRowShelf')]
+    #[ORM\JoinColumn(name:"shelf_row_shelf_code", referencedColumnName:"row_shelf_code",nullable: false)]
+    private ?Shelf $shelf_row_shelf_code = null;
+    
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'stockShelvesModules')]
+    #[ORM\JoinColumn(name:"shelf_module_code", referencedColumnName:"module_code",nullable: false)]
+    private ?Shelf $shelf_module_code = null;
+    
+    #[ORM\Id]
+    #[ORM\ManyToOne(inversedBy: 'stockShelvesBuildings')]
+    #[ORM\JoinColumn(name:"shelf_building_code", referencedColumnName:"building_code",nullable: false)]
+    private ?Shelf $shelf_building_code = null;
 
     public function getQuantity(): ?int
     {
@@ -56,6 +75,54 @@ class StockShelf
     public function setShelf(?Shelf $shelf): static
     {
         $this->shelf = $shelf;
+
+        return $this;
+    }
+
+    public function getShelfSectionCode(): ?Shelf
+    {
+        return $this->shelf_section_code;
+    }
+
+    public function setShelfSectionCode(?Shelf $shelf_section_code): static
+    {
+        $this->shelf_section_code = $shelf_section_code;
+
+        return $this;
+    }
+
+    public function getShelfRowShelfCode(): ?Shelf
+    {
+        return $this->shelf_row_shelf_code;
+    }
+
+    public function setShelfRowShelfCode(?Shelf $shelf_row_shelf_code): static
+    {
+        $this->shelf_row_shelf_code = $shelf_row_shelf_code;
+
+        return $this;
+    }
+
+    public function getShelfModeleCode(): ?Shelf
+    {
+        return $this->shelf_module_code;
+    }
+
+    public function setShelfModeleCode(?Shelf $shelf_module_code): static
+    {
+        $this->shelf_module_code = $shelf_module_code;
+
+        return $this;
+    }
+
+    public function getShelfBuildingCode(): ?Shelf
+    {
+        return $this->shelf_building_code;
+    }
+
+    public function setShelfBuildingCode(?Shelf $shelf_building_code): static
+    {
+        $this->shelf_building_code = $shelf_building_code;
 
         return $this;
     }
