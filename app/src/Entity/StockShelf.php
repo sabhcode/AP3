baddf286aba681b167318dd411225095a440be47
Hannoye Corentin <corentinhnyy@gmail.com>
2024-01-28 11:24:18 +0100

    update app

--- app/src/Entity/StockShelf.php
+++ app/src/Entity/StockShelf.php
@@ -17,7 +17,7 @@ class StockShelf
     #[ORM\ManyToOne(inversedBy: 'stockShelves')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Shelf $shelf = null;
-
+w
     #[ORM\Column]
     private ?int $quantity = null;
 
@@ -41,66 +41,6 @@ class StockShelf
     public function setShelf(?Shelf $shelf): static
     {
         $this->shelf = $shelf;
-
-        return $this;
-    }
-
-    public function getSection(): ?Shelf
-    {
-        return $this->section;
-    }
-
-    public function setSection(?Shelf $section): static
-    {
-        $this->section = $section;
-
-        return $this;
-    }
-
-    public function getWay(): ?Shelf
-    {
-        return $this->way;
-    }
-
-    public function setWay(?Way $way): static
-    {
-        $this->way = $way;
-
-        return $this;
-    }
-
-    public function getModule(): ?Shelf
-    {
-        return $this->module;
-    }
-
-    public function setModule(?Shelf $module): static
-    {
-        $this->module = $module;
-
-        return $this;
-    }
-
-    public function getBuilding(): ?Shelf
-    {
-        return $this->building;
-    }
-
-    public function setBuilding(?Shelf $building): static
-    {
-        $this->building = $building;
-
-        return $this;
-    }
-
-    public function getWarehouse(): ?Shelf
-    {
-        return $this->warehouse;
-    }
-
-    public function setWarehouse(?Shelf $warehouse): static
-    {
-        $this->warehouse = $warehouse;
 
         return $this;
     }
