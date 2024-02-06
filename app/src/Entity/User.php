af69d74f5c2d4412a0848a3e4f408ad08005a77a
Hannoye Corentin <corentinhnyy@gmail.com>
2024-02-06 15:12:59 +0100

    patch error user api

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -14,7 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ApiResource(
     operations: [
