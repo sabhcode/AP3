480a185bd8e8e1b4933d6385fa4804771f25f1cc
Hannoye Corentin <corentinhnyy@gmail.com>
2024-03-04 02:05:55 +0100

    update product API

--- app/src/Controller/API/ProductAPIController.php
+++ app/src/Controller/API/ProductAPIController.php
@@ -29,4 +29,39 @@ class ProductAPIController extends AbstractController
             ['groups' => 'product:item']
         ), json: true);
     }
+
+    #[Route(
+        '/api/product/ref',
+        'api_product_ref'
+    )]
+    public function findProductByRef(Request $request, ProductRepository $productRepository, SerializerInterface $serializer): Response
+    {
+        $supplier = strtoupper(htmlentities($request->get('supplier')));
+        $category = strtoupper(htmlentities($request->get('category')));
+        $productId = strtoupper(htmlentities($request->get('productId')));
+
+        $product = $productRepository->find($productId);
+
+        if(!$product) {
+            return new Response(status: 400);
+        }
+
+        $productSupplier = strtoupper(substr($product->getSupplier()->getName(), 0, 3));
+
+        if($productSupplier !== $supplier) {
+            return new Response(status: 400);
+        }
+
+        $productCategory = strtoupper(substr($product->getCategory()->getSlug(), 0, 3));
+
+        if($productCategory !== $category) {
+            return new Response(status: 400);
+        }
+
+        return new JsonResponse($serializer->serialize(
+            $product,
+            'json',
+            ['groups' => 'product:item']
+        ), json: true);
+    }
 }
